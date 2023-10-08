edb336564c3aedd3c7f75c35f00d649cd2b2d619
Christian Lauinger <christian.lauinger@gmail.com>
2023-10-08 11:12:03 +0200

    Seperators row moved

--- prefs.js
+++ prefs.js
@@ -558,6 +558,14 @@ class AdwPrefs {
     group1.add(adwrow);
     //row2
     adwrow = new Adw.ActionRow({
+      subtitle: _("Seperators"),
+    });
+    const showseperators = this._createGtkSwitch(settings, "show-seperators");
+    adwrow.add_suffix(showseperators);
+    adwrow.activatable_widget = showseperators;
+    group1.add(adwrow);
+    //row3
+    adwrow = new Adw.ActionRow({
       title: _("Player controls"),
     });
     const showcontrolicons = this._createGtkSwitch(
@@ -567,7 +575,7 @@ class AdwPrefs {
     adwrow.add_suffix(showcontrolicons);
     adwrow.activatable_widget = showcontrolicons;
     group1.add(adwrow);
-    //row3
+    //row4
     adwrow = new Adw.ActionRow({
       subtitle: _("Play/pause button"),
     });
@@ -578,7 +586,7 @@ class AdwPrefs {
     adwrow.add_suffix(showplaypauseicon);
     adwrow.activatable_widget = showplaypauseicon;
     group1.add(adwrow);
-    //row4
+    //row5
     adwrow = new Adw.ActionRow({
       subtitle: _("Previous track button"),
     });
@@ -586,7 +594,7 @@ class AdwPrefs {
     adwrow.add_suffix(showprevicon);
     adwrow.activatable_widget = showprevicon;
     group1.add(adwrow);
-    //row5
+    //row6
     adwrow = new Adw.ActionRow({
       subtitle: _("Next track button"),
     });
@@ -594,7 +602,7 @@ class AdwPrefs {
     adwrow.add_suffix(shownexticon);
     adwrow.activatable_widget = shownexticon;
     group1.add(adwrow);
-    //row6
+    //row7
     adwrow = new Adw.ActionRow({
       subtitle: _("Seek back button"),
     });
@@ -602,7 +610,7 @@ class AdwPrefs {
     adwrow.add_suffix(showseekback);
     adwrow.activatable_widget = showseekback;
     group1.add(adwrow);
-    //row7
+    //row8
     adwrow = new Adw.ActionRow({
       subtitle: _("Seek forward button"),
     });
@@ -613,21 +621,13 @@ class AdwPrefs {
     adwrow.add_suffix(showseekforward);
     adwrow.activatable_widget = showseekforward;
     group1.add(adwrow);
-    //row8
+    //row9
     adwrow = new Adw.ActionRow({
       title: _("Player icon"),
     });
     const showplayericon = this._createGtkSwitch(settings, "show-player-icon");
     adwrow.add_suffix(showplayericon);
     adwrow.activatable_widget = showplayericon;
-    group1.add(adwrow);
-    //row9
-    adwrow = new Adw.ActionRow({
-      title: _("Seperators"),
-    });
-    const showseperators = this._createGtkSwitch(settings, "show-seperators");
-    adwrow.add_suffix(showseperators);
-    adwrow.activatable_widget = showseperators;
     group1.add(adwrow);
     //row10
     adwrow = new Adw.ActionRow({
