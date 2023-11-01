2326afabee71ed8a30fc3308c1de1c63dd719065
TheSola10 <me@thesola.io>
2023-11-01 11:49:26 +0100

    Fix for #102

--- widget.js
+++ widget.js
@@ -307,11 +307,16 @@ export const MediaControls = GObject.registerClass(
             this.removeWidgets();
             this.disconnectSignals();
 
+            if (this.get_parent())
+                this.get_parent().remove_child(this);
+
+            /*
             for (let playerObj of Object.values(this._players)) {
                 playerObj.destroy();
             }
+            */
 
-            this.destroy();
+            //this.destroy();
         }
 
         toggleTrackInfoMenu() {
